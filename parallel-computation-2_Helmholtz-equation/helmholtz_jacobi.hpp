920eac82aa76f9ef2567e57e0cd24a7a2e4c3124
Dmitri Bogdanov <dm.bogdanov2000@gmail.com>
2022-10-27 22:11:54 +0300

    feat/Optimized away conditionals in black-red ordering, refactor/Removed deprecated vars, fix/Got rid of redundant #pragma omp's for small loops, marginally improved performance

--- parallel-computation-2_Helmholtz-equation/helmholtz_jacobi.hpp
+++ parallel-computation-2_Helmholtz-equation/helmholtz_jacobi.hpp
@@ -26,13 +26,13 @@ UniquePtrArray helholtz_jacobi(T k, std::function<T(T, T)> f, T L, size_t N, T e
 ) {
 	if (num_threads > 0) omp_set_num_threads(num_threads);
 	else  exit_with_error("num_threads must be the positive value!");
+
 	T h = T(1) / (N - 1);
 
 	const T alpha = T(4) + sqr(h) * sqr(k);
 	const T inverseAlpha = T(1) / alpha;
 	const T beta = sqr(h);
 
-	const int internalN = N - 2;
 	const int num_var = sqr(N);
 
 	auto x = make_raw_array(num_var, 0); // initial guess in zero-vector
@@ -43,25 +43,25 @@ UniquePtrArray helholtz_jacobi(T k, std::function<T(T, T)> f, T L, size_t N, T e
 
 	// Tabulate boundaries
 	// Left
-#pragma omp parallel for 
+	//#pragma omp parallel for // No significant difference noticed, seems marginally faster without OMP
 	for (int i = 0; i < N; ++i) {
 		x_ptr[i * N + 0] = BC_left(i * h);
 		x0_ptr[i * N + 0] = BC_left(i * h);
 	}
 	// Right
-#pragma omp parallel for
+	//#pragma omp parallel for // No significant difference noticed, seems marginally faster without OMP
 	for (int i = 0; i < N; ++i) {
 		x_ptr[i * N + N - 1] = BC_right(i * h);
 		x0_ptr[i * N + N - 1] = BC_right(i * h);
 	}
 	// Top
-#pragma omp parallel for
+	//#pragma omp parallel for // No significant difference noticed, seems marginally faster without OMP
 	for (int j = 1; j < N - 1; ++j) {
 		x_ptr[0 * N + j] = BC_top(j * h);
 		x0_ptr[0 * N + j] = BC_top(j * h);
 	}
 	// Bottom 
-#pragma omp parallel for
+	//#pragma omp parallel for // No significant difference noticed, seems marginally faster without OMP
 	for (int j = 1; j < N - 1; ++j) {
 		x_ptr[(N - 1) * N + j] = BC_bot(j * h);
 		x0_ptr[(N - 1) * N + j] = BC_bot(j * h);
@@ -75,9 +75,8 @@ UniquePtrArray helholtz_jacobi(T k, std::function<T(T, T)> f, T L, size_t N, T e
 		std::swap(x_ptr, x0_ptr);
 
 		// Internal loop
-	#pragma omp parallel for 
-		for (int i = 1; i < N - 1; ++i) {
 		#pragma omp parallel for 
+		for (int i = 1; i < N - 1; ++i) {
 			for (int j = 1; j < N - 1; ++j) {
 				const int indexIJ = i * N + j;
 
@@ -93,7 +92,7 @@ UniquePtrArray helholtz_jacobi(T k, std::function<T(T, T)> f, T L, size_t N, T e
 
 		// Stop condition
 		norm_diff_2 = 0;
-#pragma omp parallel for reduction(+:norm_diff_2)
+		#pragma omp parallel for reduction(+:norm_diff_2)
 		for (int i = 0; i < num_var; ++i) norm_diff_2 += sqr(x_ptr[i] - x0_ptr[i]);
 
 	} while (sqrt(norm_diff_2) > epsilon);
